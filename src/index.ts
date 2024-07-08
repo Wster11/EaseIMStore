2eb6ff780588f538c7cd1b5cafa0e2e1e9d54e86
stwang <464905943@qq.com>
2024-07-08 17:06:11 +0800

    export store

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
 import pinia from "./store/pinia";
+import { useAppUserStore } from "./store/appUser";
+import { useBlockStore } from "./store/block";
+import { useChatStore } from "./store/chat";
+import { useConnStore } from "./store/conn";
+import { useContactStore } from "./store/contact";
+import { useConversationStore } from "./store/conversation";
+import { useGroupStore } from "./store/group";
+import { useMessageStore } from "./store/message";
 
+export {
+  useAppUserStore,
+  useBlockStore,
+  useChatStore,
+  useConnStore,
+  useContactStore,
+  useConversationStore,
+  useGroupStore,
+  useMessageStore
+};
 export default pinia;
